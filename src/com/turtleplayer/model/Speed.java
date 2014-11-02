5dc4d3a1505a04b94c4f7e67bbfdd21d9104d997
elizabeth <elizabeth.shashkova@gmail.com>
2014-11-02 18:44:16 +0300

    forgotten speed file

---
+++ src/com/turtleplayer/model/Speed.java
@@ -0,0 +1,12 @@
+package com.turtleplayer.model;
+
+/**
+ * Created by elizabeth on 11/2/14.
+ */
+
+public interface Speed extends Instance
+{
+    public String getSpeedId();
+
+    public String getSpeedName();
+}
